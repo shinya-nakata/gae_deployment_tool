9cad5b7a7fc0e6c37910b1166a658d44fa0fc094
nkt <onimusha1957@gmail.com>
2017-04-01 12:22:39 +0900

    : q d

--- gae-deploy.py
+++ gae-deploy.py
@@ -20,9 +20,10 @@ def parse_args():
 class Configuration:
     def __init__(self, config_file):
         __config_file = yaml.load(config_file)
-        print(__config_file)
         self.pythonDeployTool = __config_file['pythonDeployTool']
         self.javaDeployTool = __config_file['javaDeployTool']
+        self.pythonRuntime = __config_file['pythonRuntime']
+        self.javaRuntime = __config_file['javaRuntime']
         self.apps = self.__parse_apps(__config_file['apps'])
 
     def __parse_apps(self, apps):
@@ -30,8 +31,9 @@ class Configuration:
 
     def __parse_app(self, app):
         # ランタイム環境によってデプロイツールを選択
-        deployTool = self.javaDeployTool if app['runtime'] == 'java' else self.pythonDeployTool
-        return Application(deployTool, **app)
+        deployTool = self.javaDeployTool if app['type'] == 'java' else self.pythonDeployTool
+        runtime = self.javaRuntime if app['type'] == 'java' else self.pythonRuntime
+        return Application(deployTool, runtime, **app)
 
     def get_app(self, target_app):
         for app in self.apps:
@@ -41,34 +43,61 @@ class Configuration:
             raise
 
 class Application:
-    def __init__(self, deployTool, **kwards):
+    def __init__(self, deployTool, runtime, **kwards):
         self.name = kwards['name']
         self.applicationId = kwards['applicationId']
         self.version = kwards['version']
         self.privateKey = kwards['privateKey']
         self.replaceFiles = kwards['replaceFiles']
-        self.runtime = kwards['runtime']
+        self.type = kwards['type']
         self.source = kwards['source']
         # super class
         self.deployTool = deployTool
+        self.runtime = runtime
+
+    # def deploy(self, commandLine_args):
+        #deploy_args = [self.runtime, self.deployTool, 'update']
+        # deploy_args = ["gcloud", self.deployTool, 'update']
+        # deploy_args.append(self.__get_source())
+        #
+        # applicationId = commandLine_args.a or self.applicationId
+        # if applicationId is not None:
+        #     deploy_args.append("-A")
+        #     deploy_args.append(applicationId)
+        #
+        # version = commandLine_args.v or self.version
+        # if version is not None:
+        #     deploy_args.append("-V")
+        #     deploy_args.append(version)
+        #
+        # subprocess.call(deploy_args, shell=True)
+        # print(deploy_args)
+        # pass
 
     def deploy(self, commandLine_args):
-        deploy_args = [self.deployTool]
+        # deploy_args = [self.runtime , self.runtime, "--quiet", "app", "deploy"]
+        deploy_args = [self.deployTool, "app", "deploy"]
         deploy_args.append(self.__get_source())
 
         applicationId = commandLine_args.a or self.applicationId
         if applicationId is not None:
-            deploy_args.append("-A")
-            deploy_args.append(applicationId)
+            # deploy_args.append("--project")
+            # deploy_args.append(applicationId)
+            deploy_args.append("--project=" + applicationId)
 
         version = commandLine_args.v or self.version
         if version is not None:
-            deploy_args.append("-V")
+            deploy_args.append("-v")
             deploy_args.append(version)
 
-        #subprocess.run([self.deployTool, 'update', '-A', self.applicationId, '-V', str(self.version)])
-        print(deploy_args)
-        pass
+        #
+        deploy_args.append("-q")
+
+        proc = subprocess.Popen(deploy_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        print("{}".format(deploy_args))
+        print(proc.communicate()[0])
+        # proc = subprocess.Popen(["ls", "-l"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
 
     def __get_source(self):
         if self.source['type'] == "local" : return self.source['location']
